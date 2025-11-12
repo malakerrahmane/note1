22135da6c64fc3f1d1e62a0c41177ecec409d726
malak <malakerrahmane14@gmail.com>
2025-11-12 10:56:11 +0100

    green pass test

--- test_note.py
+++ test_note.py
@@ -1,11 +1,7 @@
 import pytest
 
-def rate_note(param):
-    pass
+from note import rate_note
 
-
-def test_rate_note_is_callable():
-    rate_note(9)
 
 def test_rate_9_returns_unsuccessful():
         assert rate_note(9) == "unsuccessful"
