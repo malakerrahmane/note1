ed40bf26c3b97b5ff52605d537f86a3b2f796eee
malak <malakerrahmane14@gmail.com>
2025-11-12 20:03:19 +0100

    red failed test

--- test_note.py
+++ test_note.py
@@ -2,7 +2,7 @@ import pytest
 
 from note import rate_note
 
-@pytest.mark.parametrize("note", [8, 9])
+@pytest.mark.parametrize("note", [8,7,9])
 def test_rate_note_unsuccessful(note):
     assert rate_note(note) == "unsuccessful"
 
