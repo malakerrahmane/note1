b28bd248f2a77c5b6610e8a900a406e92b3b2e7d
malak <malakerrahmane14@gmail.com>
2025-11-12 20:09:00 +0100

    green passed test

--- note.py
+++ note.py
@@ -1,5 +1,7 @@
 def rate_note(note: int) -> str:
 
-   if note == 8 or note == 9:
-    return "unsuccessful"
-   return "acceptable"
+  if note == 7:
+     return "unsuccessful"
+  if note == 8 or note == 9:
+     return "unsuccessful"
+  return "acceptable"
