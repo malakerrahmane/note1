e03e26cb5a831d3d8800f7781999460d2bea8244
malak <malakerrahmane14@gmail.com>
2025-11-12 20:01:16 +0100

    blue refactoring

--- note.py
+++ note.py
@@ -1,6 +1,5 @@
 def rate_note(note: int) -> str:
-    if note == 8:
-        return "unsuccessful"
-    if note == 9:
-        return "unsuccessful"
-    return "acceptable"
+
+   if note == 8 or note == 9:
+    return "unsuccessful"
+   return "acceptable"
