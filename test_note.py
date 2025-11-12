338ab1da93cbc03add209629221ec26f8195f22b
malak <malakerrahmane14@gmail.com>
2025-11-12 19:57:23 +0100

    green pass test

--- test_note.py
+++ test_note.py
@@ -3,10 +3,10 @@ import pytest
 from note import rate_note
 
 def test_rate_9_returns_unsuccessful():
-        assert rate_note(9) == "unsuccessful"
+    assert rate_note(9) == "unsuccessful"
 
 def test_rate_10_returns_acceptable():
     assert rate_note(10) == "acceptable"
 
- def test_rate_8_returns_unsuccessful():
-        assert rate_note(8) == "unsuccessful"
+def test_rate_8_returns_unsuccessful():
+    assert rate_note(8) == "unsuccessful"
