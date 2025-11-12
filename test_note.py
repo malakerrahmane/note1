a954c485ff2c924e366530eaf4c23b77fc448cf2
malak <malakerrahmane14@gmail.com>
2025-11-12 20:01:47 +0100

    blue refactoring

--- test_note.py
+++ test_note.py
@@ -2,11 +2,10 @@ import pytest
 
 from note import rate_note
 
-def test_rate_9_returns_unsuccessful():
-    assert rate_note(9) == "unsuccessful"
+@pytest.mark.parametrize("note", [8, 9])
+def test_rate_note_unsuccessful(note):
+    assert rate_note(note) == "unsuccessful"
 
 def test_rate_10_returns_acceptable():
     assert rate_note(10) == "acceptable"
 
-def test_rate_8_returns_unsuccessful():
-    assert rate_note(8) == "unsuccessful"
