4f18fcede7e3219830b2e468d7a1b83d675ae2c3
malak <malakerrahmane14@gmail.com>
2025-11-12 20:29:47 +0100

    red failed test

--- note.py
+++ note.py
@@ -1,6 +1,6 @@
 def rate_note(note: int) -> str:
     if note < 10:
       return "unsuccessful"
-    if note == 10:
+    if note == 10 or note == 11:
       return "acceptable"
     return "good"
--- test_note.py
+++ test_note.py
@@ -14,3 +14,9 @@ def test_rate_12_returns_good():
 
 def test_rate_13_returns_good():
     assert rate_note(13) == "good"
+
+def test_rate_11_returns_acceptable():
+    assert rate_note(11) == "acceptable"
+
+def test_rate_14_returns_verygood():
+    assert rate_note(14) == "verygood"
